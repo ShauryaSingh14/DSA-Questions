aa0b0949d28ae281235aaee000df40253bce8447
Shaurya <shauryasingh1402@gmail.com>
2022-03-19 22:39:06 +0530

    init

--- Q54.cpp
+++ Q54.cpp
@@ -4,16 +4,13 @@ using namespace std;
 string countAndSay(int n) {
     string temp;
         unordered_map <int,int> val;
-        if(n==1){
+        if(n==1 ){
             temp ="1";
              return temp ;
         }
-
-
-        
         int i = n-1;
-        int j = n-2;
-        while( i>0 && j > 0){
+        int j = 3;
+        while( i>0 && j >0 ){
             if(i-j > 0){
                 val[i]++;
                 i-=j;
@@ -21,7 +18,6 @@ string countAndSay(int n) {
             else
                 j--;
         }
-        
         for(auto i : val){
              temp = temp + to_string(i.first) + to_string(i.second);
         }
@@ -30,7 +26,7 @@ string countAndSay(int n) {
     }
 int main()
 {
-    int N = 3;
+    int N = 2;
     cout << countAndSay(N) << endl;
     return 0;
 }
