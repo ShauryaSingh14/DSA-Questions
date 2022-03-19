19954d538f32502bc13ab32b781168526957aa92
Shaurya <shauryasingh1402@gmail.com>
2022-03-19 20:33:55 +0530

    init

---
+++ Q54.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+string countAndSay(int n) {
+    string temp;
+        unordered_map <int,int> val;
+        if(n==1){
+            temp ="1";
+             return temp ;
+        }
+
+
+        
+        int i = n-1;
+        int j = n-2;
+        while( i>0 && j > 0){
+            if(i-j > 0){
+                val[i]++;
+                i-=j;
+            }
+            else
+                j--;
+        }
+        
+        for(auto i : val){
+             temp = temp + to_string(i.first) + to_string(i.second);
+        }
+        
+        return temp;
+    }
+int main()
+{
+    int N = 3;
+    cout << countAndSay(N) << endl;
+    return 0;
+}
